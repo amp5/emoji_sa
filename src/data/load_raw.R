b1cf0881733addaa3bb3252e0e2942fc5fb75de6
amp5 <aplassaras@gmail.com>
2017-01-31 17:09:11 -0500

    adding notes and first drafts of report

---
+++ src/data/load_raw.R
@@ -0,0 +1,14 @@
+#########################################################################
+#    File_name: load_raw.R
+#    Version: R version 3.2.4 (2016-03-10)
+#    Date: 01_30_2017
+#    Author: amp5
+#    Purpose: loading raw data and saving it into a data frame. 
+#    Input_files:
+#    Output_files:
+#    Previous_files: N/A
+#    Dependencies:
+#    Required_by"
+#    Status:
+#    Machine: OSX Yosemite v. 10.10.5 (laptop)
+#########################################################################
