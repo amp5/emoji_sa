27088115b5c890fd6caccb136d1da3ad42af300e
amp5 <aplassaras@gmail.com>
2017-02-26 00:59:53 -0500

    added emoji dicts and working on script

--- src/data/extract_emoji_tw.R
+++ src/data/extract_emoji_tw.R
@@ -13,15 +13,77 @@
 #########################################################################
 
 library(tidyverse)
+library(plyr)
+library(ggplot2)
+library(splitstackshape)
+library(stringr)
 # instead of reloading input files, working with dataframe wrk_d which was
 # saved as non_USfiltered.csv
 
 wrk_d <- read.csv(file.choose())
 
 
-samp <- head(wrk_d$text, 20)
+tweets <- data.frame(head(wrk_d, 20))
+tweets <- wrk_d
 
 # From looking at sample of tweets, emojis display as format -> \U0001f602
 
+#### READ IN EMOJI DICTIONARIES
+setwd("/Users/alexandraplassaras/Desktop/Spring_2017/QMSS_Thesis/QMSS_thesis/data/external")
+emdict.la <- read.csv('emoticon_conversion_noGraphic.csv', header = F); #Lauren Ancona; https://github.com/laurenancona/twimoji/tree/master/twitterEmojiProject
+emdict.la <- emdict.la[-1, ]; row.names(emdict.la) <- NULL; names(emdict.la) <- c('unicode', 'bytes', 'name'); emdict.la$emojiid <- row.names(emdict.la);
+emdict.jpb <- read.csv('emDict.csv', header = F) #Jessica Peterka-Bonetta; http://opiateforthemass.es/articles/emoticons-in-R/
+emdict.jpb <- emdict.jpb[-1, ]; row.names(emdict.jpb) <- NULL; names(emdict.jpb) <- c('name', 'bytes', 'rencoding'); emdict.jpb$name <- tolower(emdict.jpb$name);
+emdict.jpb$bytes <- NULL;
+## merge dictionaries
+emojis <- merge(emdict.la, emdict.jpb, by = 'name');  emojis$emojiid <- as.numeric(emojis$emojiid); emojis <- arrange(emojis, emojiid);
 
+setwd("/Users/alexandraplassaras/Desktop/Spring_2017/QMSS_Thesis/QMSS_thesis")
+###### FIND TOP EMOJIS FOR A GIVEN SUBSET OF THE DATA
+
+# tweets <- subset(tweets.final, hashtag %in% c('#womensmarch'));
+## create full tweets by emojis matrix
+df.s<- matrix(NA, nrow = nrow(tweets), ncol = ncol(emojis)); 
+system.time(df.s <- sapply(emojis$rencoding, regexpr, tweets$text, ignore.case = T, useBytes = T));
+
+
+
+
+rownames(df.s) <- 1:nrow(df.s); 
+colnames(df.s) <- 1:ncol(df.s); 
+df.t <- data.frame(df.s); 
+df.t$tweetid <- tweets$X.1;
+
+# merge in hashtag data from original tweets dataset
+df.a <- subset(tweets, select = c(tweetid, hashtag)); 
+df.u <- merge(df.t, df.a, by = 'tweetid'); 
+df.u$z <- 1; 
+df.u <- arrange(df.u, tweetid); 
+tweets.emojis.matrix <- df.t
+## create emoji count dataset
+df <- subset(tweets.emojis.matrix)[, c(2:843)]; 
+count <- colSums(df > -1);
+emojis.m <- cbind(count, emojis); 
+emojis.m <- arrange(emojis.m, desc(count));
+
+emojis.count <- subset(emojis.m, count > 1); 
+emojis.count$dens <- round(1000 * (emojis.count$count / nrow(tweets)), 1); 
+emojis.count$dens.sm <- (emojis.count$count + 1) / (nrow(tweets) + 1);
+
+emojis.count$rank <- as.numeric(row.names(emojis.count));
+emojis.count.p <- subset(emojis.count, select = c(name, dens, count, rank));
+
+# print summary stats
+subset(emojis.count.p, rank <= 10);
+num.tweets <- nrow(tweets); 
+df.t <- rowSums(tweets.emojis.matrix[, c(2:843)] > -1); 
+num.tweets.with.emojis <- length(df.t[df.t > 0]); 
+num.emojis <- sum(emojis.count$count);
+
+
+num.tweets; 
+num.tweets.with.emojis; 
+round(100 * (num.tweets.with.emojis / num.tweets), 1); 
+num.emojis; 
+nrow(emojis.count)
 
