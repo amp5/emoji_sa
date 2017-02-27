6ee566c0fa99fc03df4354cefe0d0ada169a3565
amp5 <aplassaras@gmail.com>
2017-02-27 00:04:48 -0500

    have emoji tweets separated

--- src/features/extract_emoji_tw.R
+++ src/features/extract_emoji_tw.R
@@ -32,14 +32,21 @@ tweets <- wrk_d
 # for now just concered with text and an id for each tweet
 tweets <- tweets[, c("X.1", "text")]
 
-
 ###### FIND TOP EMOJIS FOR A GIVEN SUBSET OF THE DATA
 # From looking at sample of tweets, emojis display as format -> \U0001f602
 # however R encodes this differently, must convert it
 tweets$text_n_url <- gsub('http.*\\s*', '', tweets$text)
-tweets$emoji <- gsub("[a-zA-Z @!#$%^&*()_+=:?.,]", "", tweets$text_n_url)
-tweets$emoji_conv <- as.factor(iconv(tweets$emoji, "latin1", "ASCII", "byte"))
+tweets$emoji <- gsub("[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ @!#$%^&*()|_+=:?.,;1234567890-]", "", tweets$text_n_url)
+#tweets$emoji_conv <- as.factor(iconv(tweets$emoji, "latin1", "ASCII", "byte"))
+tweets$emoji <- noquote(tweets$emoji)
+tweets$emoji <- gsub("'", '', tweets$emoji)
+tweets$emoji <- gsub("''", '', tweets$emoji)
+tweets$emoji <- sub("/", "", tweets$emoji)
+tweets$emoji <- sub("//", "", tweets$emoji) 
 
+emoji_twts <- tweets[!(tweets$emoji == ""),]
+
+############################################### STOPPED HERE 2/27/17 MIDNIGHT #########################
 ## create full tweets by emojis matrix
 emoji.fv <- vapply(emojis$bytes, regexpr,FUN.VALUE = integer(nrow(tweets)),
                    tweets$emoji, useBytes = T )
@@ -47,6 +54,11 @@ rownames(emoji.fv) <- 1:nrow(emoji.fv);
 colnames(emoji.fv) <- 1:ncol(emoji.fv); 
 ## count is off
 df.t <- data.frame(emoji.fv); 
+
+
+#count row sum and if row has no positive numbers don't save in new list
+ 
+
 
 ## create emoji count dataset
 df <- subset(df.t)[, c(1:842)]; 
@@ -60,6 +72,7 @@ emoji_c$dens <- round(1000 * (emoji_c$count / nrow(tweets)), 1);
 emoji_c$dens_sm <- (emoji_c$count + 1) / (nrow(tweets) + 1);
 
 #add ranking section later
+# order decscending percentage then do head for top 10
 #emoji_c$rank <- as.numeric(row.names(emoji_c));
 #emoji_cp <- subset(emoji_p, select = c(name, dens, count, rank));
 
