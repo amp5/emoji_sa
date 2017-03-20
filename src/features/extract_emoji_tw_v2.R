9ca03ba9dc2291e356ce2cf478a3db2377877f78
amp5 <aplassaras@gmail.com>
2017-03-20 00:01:59 -0400

    added more images and updated EDA analysis

---
+++ src/features/extract_emoji_tw_v2.R
@@ -0,0 +1,120 @@
+#########################################################################
+#    File_name: extract_emoji_tw_v2.R
+#    Version: R version 3.2.4 (2016-03-10)
+#    Date: 03_19_2017
+#    Author: amp5
+#    Purpose: Identify which tweets have emojis in them and which do not. 
+#             From there filter out all tweets with at least one emoji.
+#             - separate out just tweets and ids then merge back with big tibble at end
+#             - figure out how many unique users and how many tweets after only emoji extracted
+#             REDO analysis using just e-tweets with party affiliations
+#    Input_files: filtered.csv, full_emoji_db.csv
+#    Output_files: emoji_counts_v2.csv
+#    Previous_files: emoji_databases.R and refine_dataset.R
+#    Required by: exploratoy_data_analysis.R
+#    Status: Completed
+#    Machine: OSX Yosemite v. 10.10.5 (laptop)
+#########################################################################
+library(tidyverse)
+library(plyr)
+library(ggplot2)
+library(splitstackshape)
+library(stringr)
+
+# Setting path and loading files ------------------------------------------
+path <- "/Users/alexandraplassaras/Desktop/Spring_2017/QMSS_Thesis/QMSS_thesis" 
+setwd(path)
+
+# load e_twts_w_party.Rda, var name is 'only_party'
+load(file.choose())
+
+# load full_emoji_db.csv
+emojis <- read.csv(file.choose())
+
+
+# Code --------------------------------------------------------------------
+d <- as_tibble(only_party)
+e <- as_tibble(emojis)
+
+#tst <- head(d, 25)
+#tweets <- d[12:14,]
+#tweets <- d[12,]
+#tweets <- d[1:25,]
+tweets <- d
+
+# for now just concered with text and an id for each tweet
+tweets <- tweets[, c("X", "text.x")]
+names(tweets) <- c("X", "text")
+
+###### FIND TOP EMOJIS FOR A GIVEN SUBSET OF THE DATA
+# From looking at sample of tweets, emojis display as format -> \U0001f602
+# however R encodes this differently, must convert it
+twts <- tweets %>%
+  mutate(text_n_url = gsub('http.*\\s*', '', text)) %>%
+  mutate(emoji = gsub("[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ @!#$%^&*()|_+=:?.,;1234567890-]", "", text_n_url)) 
+
+twts$emoji <- gsub("''", '', twts$emoji)
+twts$emoji <- sub("//", "", twts$emoji)
+twts$emoji <- gsub("'", '', twts$emoji)
+twts$emoji <- sub("/", "", twts$emoji)
+
+twts <- as_tibble(twts)
+e_twts <- filter(twts, !(twts$emoji == ""))
+
+e_twts <- e_twts %>%
+  mutate(singl_emoji = strsplit(emoji, "")) %>%
+  mutate(num = map_int(singl_emoji, length)) %>%
+  unnest()
+e_twts$internal_id <- 1:nrow(e_twts)
+
+## create full tweets by emojis matrix
+has_emoji <- vapply(e$bytes, regexpr, FUN.VALUE = integer(nrow(e_twts)),
+                    e_twts$singl_emoji, useBytes = T )
+
+rownames(has_emoji) <- 1:nrow(has_emoji)
+colnames(has_emoji) <- 1:ncol(has_emoji) 
+emoji_twts <- tibble::as_data_frame(has_emoji)
+
+count <- colSums(has_emoji > -1)
+emojis_m <- cbind(count, e) 
+emojis_m <- arrange(emojis_m, desc(count))
+
+emoji_c <- subset(emojis_m, count > 0)
+emoji_c$dens <- round(1000 * (emoji_c$count / nrow(twts)), 1) 
+emoji_c$dens_sm <- (emoji_c$count + 1) / (nrow(twts) + 1)
+
+
+emoji_ids <- tibble::as_data_frame(which(emoji_twts > 0, arr.ind = T) )
+result <- merge(e_twts, emoji_ids, by.x = "internal_id", by.y = "row", all.x = TRUE)
+colnames(result)[8] <- "emoji_id"
+
+
+result <- result %>%
+  filter(!(is.na(result$emoji_id))) %>%
+  group_by(X, text, emoji) %>% 
+  nest(emoji_id)
+
+
+(num_twts <- nrow(twts) )
+(num_tweets_w_emojis <- nrow(result))
+(num_emojis <- sum(emoji_c$count))
+(num_types_emojis <- nrow(emoji_c))
+round(100 * (num_tweets_w_emojis / num_twts), 1)
+
+
+#add ranking section later
+# order decscending percentage then do head for top 10
+#emoji_c$rank <- as.numeric(row.names(emoji_c));
+#emoji_cp <- subset(emoji_p, select = c(name, dens, count, rank));
+
+# print summary stats
+#subset(emojis.count.p, rank <= 10);  
+
+
+
+
+
+# Creating outputs  -------------------------------------------------------
+write.csv(emoji_c, "data/interim/emoji_counts_v2.csv")
+
+
--- src/visualizations/exploratory_data_analysis.R
+++ src/visualizations/exploratory_data_analysis.R
@@ -27,9 +27,17 @@ setwd(path)
 # load extracted_emojis.Rda, var name is 'final'
 load(file.choose())
 
+# load e_twts_w_party.Rda, var name is 'only_party'
+load(file.choose())
+
+# load filtered.csv
+all_twts <- read.csv(file.choose())
+
 # emoji_counts.csv
 emoji_typ <- read.csv(file.choose())
 
+# emoji_counts_v2.csv
+emoji_typ2 <- read.csv(file.choose())
 # Code --------------------------------------------------------------------
 wrk_d <- final
 
@@ -66,6 +74,7 @@ e_typ <- e_typ %>%
 
 
 top_10 <- head(e_typ, 10)
+top_10$group <-  "emoji_tweets"
 
 top_10 %>%
   ggplot(mapping = aes(x = reorder(name, percent), y = percent)) +
@@ -73,24 +82,85 @@ top_10 %>%
   coord_flip() +
   geom_text(aes(label=round(percent, digits = 2)), vjust="inward", hjust = -.1, color="black", size=3.5)+
   theme_minimal() +
-  ggtitle("Most Commonly Used Emojis") +
+  ggtitle("Most Commonly Used Emojis in Emoji Tweets") +
   labs(x = "Emoji Names", y = "Percent (%)")
 ggsave("reports/figures/commonly_used_e.pdf")
 
-# Plot tweets by date:
-twt_date <- as_tibble(table(factor(format(wrk_d$created))))
-names(twt_date) <- c("date", "num")
-twt_date$date <- as.Date(mdy(twt_date$date, tz = "America/New_York"))
+# ----------
 
-ggplot(twt_date, aes(x=date, y = num)) + 
-  geom_line(colour = "#999999") +
-  geom_point(colour="#1DA1F2") + 
+e_typ2 <- subset(emoji_typ2, select=-c(X.1, bytes, rencoding, X))
+e_typ2 <- e_typ2[c(4, 3, 2, 1, 5, 6)]
+e_typ2 <- as_tibble(e_typ2)
+e_typ2 <- e_typ2 %>%
+  mutate(percent = count / e_twts_num)
+
+
+top_10_2 <- head(e_typ2, 10)
+top_10_2$group <-  "e_twts_p"
+
+top_10_2 %>%
+  ggplot(mapping = aes(x = reorder(name, percent), y = percent)) +
+  geom_bar(stat="identity") +
+  coord_flip() +
+  geom_text(aes(label=round(percent, digits = 2)), vjust="inward", hjust = -.1, color="black", size=3.5)+
+  theme_minimal() +
+  ggtitle("Most Commonly Used Emojis in Emoji Tweets with Party Affiliation") +
+  labs(x = "Emoji Names", y = "Percent (%)")
+ggsave("reports/figures/commonly_used_e_p.pdf")
+
+
+
+
+
+# Plot tweets by date:
+twt_date_e <- as_tibble(table(factor(format(wrk_d$created))))
+names(twt_date_e) <- c("date", "num")
+twt_date_e$date <- as.Date(mdy(twt_date_e$date, tz = "America/New_York"))
+twt_date_e$group <- "only_emoji"
+
+twt_date_p <- as_tibble(table(factor(format(only_party$created))))
+names(twt_date_p) <- c("date", "num")
+twt_date_p$date <- as.Date(mdy(twt_date_p$date, tz = "America/New_York"))
+twt_date_p$group <- "emoji_w_party"
+
+
+
+twt_date_a <- as_tibble(table(factor(format(all_twts$created))))
+names(twt_date_a) <- c("date", "num")
+twt_date_a$date <- as.Date(mdy(twt_date_a$date, tz = "America/New_York"))
+twt_date_a$group <- "all_tweets"
+
+twt_date <- rbind(twt_date_e, twt_date_p)
+
+
+# overall tweets - #999999
+# e_tweets - #1DA1F2
+# party tweets - purple color...
+
+c('#1DA1F2', "#999999" )
+# compare tweets, e_tweets and party_twts
+ggplot(twt_date, aes(x=date, y = num, color = group)) + 
+  geom_line() +
+  geom_point() + 
   theme_minimal() +
   theme(axis.text.x=element_text(angle=90)) +
-  scale_x_date(date_breaks = ("4 day"), date_labels = "%b %d") +
-  ggtitle("Emoji Tweets by Date") +
+  scale_x_date(date_breaks = ("5 day"), date_labels = "%b %d") +
+  ggtitle("Tweets by Date - Emoji Tweets vs. Emoji Tweets with Party") +
   labs(x = "Date", y = "Count of Tweets")
-ggsave("reports/figures/twts_by_date.pdf")
+ggsave("reports/figures/twts_by_date_e_p.pdf")
+
+
+
+
+ggplot(twt_date_a, aes(x=date, y = num)) + 
+  geom_line(colour = "#1DA1F2") +
+  geom_point(colour = "#1DA1F2") + 
+  theme_minimal() +
+  theme(axis.text.x=element_text(angle=90)) +
+  scale_x_date(date_breaks = ("5 day"), date_labels = "%b %d") +
+  ggtitle("All Tweets by Date") +
+  labs(x = "Date", y = "Count of Tweets")
+ggsave("reports/figures/twts_by_date_a.pdf")
 
 # Word Cloud --------------------------------------------------------------
 tc <- function(filename){
@@ -140,6 +210,9 @@ tc <- function(filename){
 wc_t <- function(filename){
   return(wordcloud(filename, min.freq = 400,  max.words = 1000, random.order = FALSE, colors = brewer.pal(4, "Dark2")))
 }
+
+names(only_party)[2] <-  "test"
+wrk_d <- only_party
 
 wc_t(tc(wrk_d))
 
